c8787c7f35ac0a8713ccc286de8a3812763cb640
unknown <lautaro_t11@hotmail.com>
2026-02-27 23:13:53 -0300

    express static

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import Productsroute from './routes/products.routes.js'
 import cartsRoute from './routes/carts.routes.js'
+import __dirname from './utils.js'
 
 const app = express()
 app.use(express.json())
+app.use(express.static(__dirname + '/public'))
+
 app.use('/api/products', Productsroute)
 app.use('/api/carts', cartsRoute)
 
